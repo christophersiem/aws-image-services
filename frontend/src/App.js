7c63d3c8d1e1ad72fb201a51c7df3499833a1364
christophersiem <christopher.siem@gmail.com>
2020-12-02 23:25:51 +0100

    Add Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,37 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './App.css';
-import {uploadImage} from "./service/upload-service";
+
 import styled from 'styled-components/macro'
-import ImageUpload from "./components/ImageUpload";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from "react-router-dom";
+
+import ImageUploadPage from "./pages/ImageUploadPage";
+import Home from "./pages/Home";
+
 
 function App() {
-    const [image, setImage] = useState();
-    const [imageUrl, setImageUrl] = useState();
+
     return (
-        <WrapperStyled>
-            <HeadingStyled>This is my Super Duper Uploader</HeadingStyled>
-            <ImageUpload
-                handleImage={handleImage}
-                upload={upload}
-                image={image}
-            />
-            <p>{imageUrl && "Image successfully uploaded"}</p>
-        </WrapperStyled>
-
+        <Router>
+            <WrapperStyled>
+                <Switch>
+                    <Route path="/home" component={Home}/>
+                    <Route path="/upload" component={ImageUploadPage}/>
+                    <Route path="/"><Redirect to="/home"/> </Route>
+                </Switch>
+            </WrapperStyled>
+        </Router>
     )
-
-    function handleImage(event) {
-        const file = event.target.files[0];
-        setImage(file);
-    }
-
-    function upload() {
-        uploadImage(image).catch(e => console.error(e)).then(setImageUrl)
-    }
-
 }
 
-const HeadingStyled = styled.h1`
-text-align: center;
-font-size: 1.5rem;
-`
 const WrapperStyled = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
-align-content: space-around;
-
 `
 
 export default App;
