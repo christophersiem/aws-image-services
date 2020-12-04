7d93514ac812dbf583626a90449b563487ed1d03
christophersiem <christopher.siem@gmail.com>
2020-12-04 01:09:36 +0100

    Style and Refactor

--- frontend/src/components/ImageUpload.js
+++ frontend/src/components/ImageUpload.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import styled from 'styled-components/macro'
 
 export default function ImageUpload({handleImage, upload, image}) {
     return (
         <>
-            <input type={"file"} accept={"image/*"} onChange={handleImage} />
+            <InputStyled type={"file"} title={" "} accept={"image/*"} onChange={handleImage} />
             <button disabled={!image} onClick={upload}>Upload</button>
         </>
     )
 }
+
+const InputStyled = styled.input`
+margin: auto;
+`
+
 
 
--- frontend/src/pages/ImageUploadPage.js
+++ frontend/src/pages/ImageUploadPage.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import ImageUpload from "../components/ImageUpload";
-import {uploadImage} from "../service/upload-service";
 import Heading from "../commons/Heading";
-
+import styled from "styled-components/macro";
+import { uploadImage} from "../service/upload-service";
 
 export default function ImageUploadPage() {
     const [image, setImage] = useState();
@@ -10,13 +10,13 @@ export default function ImageUploadPage() {
     const [showImage, setShowImage] = useState(false)
 
     return (
-        <>
-            <Heading heading={"This is my Super Duper Uploader"}/>
+        <WrapperStyled>
+            <Heading heading={"This is my Super Duper Image - to - AWS - Uploader"}/>
             <ImageUpload handleImage={handleImage} upload={upload} image={image}/>
             {imageUrl && <p>Image successfully uploaded to AWS</p>}
             <button disabled={!imageUrl} onClick={()=>setShowImage(!showImage)}>Show / Hide Image</button>
-            {showImage && <img src={imageUrl} alt=""/>}
-        </>
+            {showImage && <ImageStyled src={imageUrl} alt=""/>}
+        </WrapperStyled>
     )
 
     function handleImage(event) {
@@ -29,3 +29,15 @@ export default function ImageUploadPage() {
     }
 };
 
+const WrapperStyled = styled.div`
+display: grid;
+grid-template-columns: 1fr;
+grid-template-rows: 1fr 1fr 0.5fr 0.5fr max-content;
+justify-items: center;
+gap: 10px 0;
+`
+
+const ImageStyled = styled.img`
+max-width: 90%;
+height: auto;
+`
