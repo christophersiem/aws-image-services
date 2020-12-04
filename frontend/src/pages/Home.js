fe90c560e3626648d4da7dd13f5cc88ddc75672d
christophersiem <christopher.siem@gmail.com>
2020-12-04 01:10:14 +0100

    Add second button

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Heading from "../commons/Heading";
 import {useHistory} from "react-router-dom";
 
+
 export default function Home() {
     const history = useHistory();
     return (
-        <>
-            <Heading heading={"Welcome on my Super Duper Page"}/>
+        <div>
+            <Heading heading={"Welcome on my Super Duper AWS Page"}/>
             <p>Soon you will be able to check out different AWS Services here</p>
             <button onClick={() => history.push("/upload")}>1. Upload a Picture to AWS</button>
-        </>
+            <button onClick={() => history.push("/face")}>2. Analyze a face</button>
+        </div>
     )
 };
+
+
